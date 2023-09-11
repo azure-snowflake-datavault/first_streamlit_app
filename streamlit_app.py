6f6308a70ff39576c4febcdfff052aa580eb95cf
azure-snowflake-datavault <144713879+azure-snowflake-datavault@users.noreply.github.com>
2023-09-11 22:21:01 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,25 +3,19 @@ import sqlalchemy
 import pyodbc
 
 st.title('Bank')
-
-
 st.text('Account Name')
 st.text('Account Balance')
 st.text('Rrfresh Balance')
-
-
-
 st.text('Send money')
 st.text('Recipient')
-
 st.text('Request money')
 st.text('Requestee')
 
-st.text('Test 2')
+st.text('Test 5')
 
 
-conn = st.experimental_connection('streamlit_bank', type='sql')
-Customer = conn.query('select * from dbo.Customer')
-st.dataframe(Customer)
+# conn = st.experimental_connection('streamlit_bank', type='sql')
+# Customer = conn.query('select * from dbo.Customer')
+# st.dataframe(Customer)
 
 
