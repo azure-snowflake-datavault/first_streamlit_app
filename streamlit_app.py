849f1e37ff70cf8062fb91a36509babbb120e0d5
azure-snowflake-datavault <144713879+azure-snowflake-datavault@users.noreply.github.com>
2023-09-11 20:16:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,19 @@
+import streamlit
+
+streamlit.title('Bank')
+
+
+streamlit.text('Account Name')
+streamlit.text('Account Balance')
+streamlit.text('Rrfresh Balance')
+
+
+
+streamlit.text('Send money')
+streamlit.text('Recipient')
+
+streamlit.text('Request money')
+streamlit.text('Requestee')
+
+
 
