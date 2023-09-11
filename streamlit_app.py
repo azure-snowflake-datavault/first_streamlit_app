93305b652059e375dd41503acf88e2e9340539b7
azure-snowflake-datavault <144713879+azure-snowflake-datavault@users.noreply.github.com>
2023-09-11 20:49:56 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,10 @@ st.text('Recipient')
 st.text('Request money')
 st.text('Requestee')
 
+st.text('Test 2')
+
 
 conn = st.experimental_connection('streamlit_bank', type='sql')
-
 Customer = conn.query('select * from dbo.Customer')
 st.dataframe(Customer)
 
